a6863c8c1ba630f32e56bfda2f1a62e7976c8fd9
tirathpatel <tirath_24@hotmail.com>
2016-07-23 14:05:13 -0400

    Initial Commit

---
+++ WalkALot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WalkALot
+//
+//  Created by Tirath Patel on 2016-07-23.
+//  Copyright © 2016 Tirath Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
